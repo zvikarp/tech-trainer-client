b65ce2472b593cb136db25270701c54df8994808
board2675 <board2675@gmail.com>
2019-07-08 15:22:17 +0300

    -

--- src/pages/Welcome/Welcome.js
+++ src/pages/Welcome/Welcome.js
@@ -4,12 +4,27 @@ import "../../utils/styles/global.css";
 import "./Welcome.css";
 
 class Welcome extends Component {
+    constructor(props) {
+        super(props);
+        this.state({
+            signup: true,
+        });
+    }
+
+    renderForm() {
+        if (this.state.signup) {
+            return (
+                <Signup />
+                <div class="link-to-signin">already a user? <a href="/">sign in here</a>.</div>
+            );
+        }
+    }
+
     render() {
         return (
             <div id="welcome">
                 <h1 class="welcome-message">welcome to orange chat  <span role="img" aria-label="banana">🧡📪</span></h1>
-                <Signup />
-                <div class="link-to-signin">already a user? <a href="/">sign in here</a>.</div>
+                
             </div>
         )
     }
